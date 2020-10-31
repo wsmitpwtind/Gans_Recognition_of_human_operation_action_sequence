4d733b50a9fda008956036a879cb26dc7869dbfa
wsmitpwtind <wsmitpwtind@163.com>
2020-10-31 20:31:46 +0800

    删除冗余语句+试图修一个不影响运行的小bug未果

--- Train.py
+++ Train.py
@@ -21,7 +21,7 @@ def to_img(x):
 
 batch_size = 128
 num_epoch = 100
-z_dimension = 100  # noise dimension
+z_dimension = 100
 
 img_transform = transforms.Compose([
     transforms.ToTensor(),
@@ -37,14 +37,14 @@ class discriminator(nn.Module):
     def __init__(self):
         super(discriminator, self).__init__()
         self.conv1 = nn.Sequential(
-            nn.Conv2d(1, 32, 5, padding=2),  # batch, 32, 28, 28
+            nn.Conv2d(1, 32, 5, padding=2),
             nn.LeakyReLU(0.2, True),
-            nn.AvgPool2d(2, stride=2),  # batch, 32, 14, 14
+            nn.AvgPool2d(2, stride=2),
         )
         self.conv2 = nn.Sequential(
-            nn.Conv2d(32, 64, 5, padding=2),  # batch, 64, 14, 14
+            nn.Conv2d(32, 64, 5, padding=2),
             nn.LeakyReLU(0.2, True),
-            nn.AvgPool2d(2, stride=2)  # batch, 64, 7, 7
+            nn.AvgPool2d(2, stride=2)
         )
         self.fc = nn.Sequential(
             nn.Linear(64 * 7 * 7, 1024),
@@ -54,9 +54,7 @@ class discriminator(nn.Module):
         )
 
     def forward(self, x):
-        '''
-        x: batch, width, height, channel=1
-        '''
+
         x = self.conv1(x)
         x = self.conv2(x)
         x = x.view(x.size(0), -1)
@@ -67,23 +65,23 @@ class discriminator(nn.Module):
 class generator(nn.Module):
     def __init__(self, input_size, num_feature):
         super(generator, self).__init__()
-        self.fc = nn.Linear(input_size, num_feature)  # batch, 3136=1x56x56
+        self.fc = nn.Linear(input_size, num_feature)
         self.br = nn.Sequential(
             nn.BatchNorm2d(1),
             nn.ReLU(True)
         )
         self.downsample1 = nn.Sequential(
-            nn.Conv2d(1, 50, 3, stride=1, padding=1),  # batch, 50, 56, 56
+            nn.Conv2d(1, 50, 3, stride=1, padding=1),
             nn.BatchNorm2d(50),
             nn.ReLU(True)
         )
         self.downsample2 = nn.Sequential(
-            nn.Conv2d(50, 25, 3, stride=1, padding=1),  # batch, 25, 56, 56
+            nn.Conv2d(50, 25, 3, stride=1, padding=1),
             nn.BatchNorm2d(25),
             nn.ReLU(True)
         )
         self.downsample3 = nn.Sequential(
-            nn.Conv2d(25, 1, 2, stride=2),  # batch, 1, 28, 28
+            nn.Conv2d(25, 1, 2, stride=2),
             nn.Tanh()
         )
 
@@ -97,49 +95,36 @@ class generator(nn.Module):
         return x
 
 
-D = discriminator().cuda()  # discriminator model
-G = generator(z_dimension, 3136).cuda()  # generator model
+D = discriminator().cuda()
+G = generator(z_dimension, 3136).cuda()
 
-criterion = nn.BCELoss()  # binary cross entropy
+criterion = nn.BCELoss()
 
 d_optimizer = torch.optim.Adam(D.parameters(), lr=0.0003)
 g_optimizer = torch.optim.Adam(G.parameters(), lr=0.0003)
 
-# train
 for epoch in range(num_epoch):
     for i, (img, _) in enumerate(dataloader):
         num_img = img.size(0)
-        # =================train discriminator
         real_img = Variable(img).cuda()
         real_label = Variable(torch.ones(num_img)).cuda()
         fake_label = Variable(torch.zeros(num_img)).cuda()
-
-        # compute loss of real_img
         real_out = D(real_img)
         d_loss_real = criterion(real_out, real_label)
-        real_scores = real_out  # closer to 1 means better
-
-        # compute loss of fake_img
+        real_scores = real_out
         z = Variable(torch.randn(num_img, z_dimension)).cuda()
         fake_img = G(z)
         fake_out = D(fake_img)
         d_loss_fake = criterion(fake_out, fake_label)
-        fake_scores = fake_out  # closer to 0 means better
-
-        # bp and optimize
+        fake_scores = fake_out
         d_loss = d_loss_real + d_loss_fake
         d_optimizer.zero_grad()
         d_loss.backward()
         d_optimizer.step()
-
-        # ===============train generator
-        # compute loss of fake_img
         z = Variable(torch.randn(num_img, z_dimension)).cuda()
         fake_img = G(z)
         output = D(fake_img)
         g_loss = criterion(output, real_label)
-
-        # bp and optimize
         g_optimizer.zero_grad()
         g_loss.backward()
         g_optimizer.step()
